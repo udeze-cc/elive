d61fa380485f12e79a62518769d5875f9365afbf
udeze-cc <udeze.cc@gmail.com>
2023-09-21 01:08:30 +0100

    Back Button

--- src/adminPage.js
+++ src/adminPage.js
@@ -9,7 +9,6 @@ import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import Button from '@mui/material/Button';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
